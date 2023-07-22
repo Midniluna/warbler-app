0cb26ca37a6783bb1ad84c86cfcdce268e646d16
root <midnightlunalux@gmail.com>
2023-07-22 16:01:42 -0400

    Tests for message model; Success

--- test_message_model.py
+++ test_message_model.py
@@ -9,7 +9,7 @@ import os
 from unittest import TestCase
 from flask import g
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Likes
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -38,9 +38,57 @@ class MessageModelTestCase(TestCase):
 
         User.query.delete()
         Message.query.delete()
-        Follows.query.delete()
+        Likes.query.delete()
 
         self.client = app.test_client()
 
+        u = User.signup("testuser", "test@test.com", "password", None
+        )
+
+        db.session.add(u)
+        db.session.commit()
+
+        self.user = u
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+    
+
     def test_message_model(self):
-        """Conform message model works as intended"""
+        """Confirm message model works as intended"""
+
+        msg = Message(
+            text = "Mmm content",
+            user_id = self.user.id
+        )
+
+        db.session.add(msg)
+        db.session.commit()
+
+        self.assertEqual(len(self.user.messages), 1)
+        self.assertEqual(self.user.messages[0].text, "Mmm content")
+
+    def test_message_likes(self):
+        """Confirm liking a message works as intended"""
+
+        user2 = User.signup("anotheruser", "email@em.com", "password", None)
+
+        db.session.add(user2)
+        db.session.commit()
+
+        msg = Message(
+            text = "Wowie new message",
+            user_id = self.user.id
+        )
+
+        db.session.add(msg)
+        db.session.commit()
+
+        user2.likes.append(msg)
+        db.session.commit()
+
+        l = Likes.query.filter(Likes.user_id == user2.id).all()
+        self.assertEqual(len(l), 1)
+        self.assertEqual(l[0].message_id, msg.id)
